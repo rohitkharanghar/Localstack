37a12a365994b9240799b556e0097eb82043f0e3
Rohit Kharanghar <rohitkharanghar@gmail.com>
2025-01-31 17:22:12 +0530

    terraform for serverless

---
+++ terraform/opensearch-serverless/variables.tf
@@ -0,0 +1,9 @@
+variable "collection_name" {
+  description = "Name of the OpenSearch Serverless collection."
+  default     = "example-collection"
+}
+
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
